75e73a4cfda796bb2cd9995b1f64ae5f1bddb82b
inodinwetrust10 <adi4gbsingh@gmail.com>
2024-06-21 19:35:22 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mumblebackend-production.up.railway.app/",
+        target: "https://mumblebackend-production.up.railway.app",
       },
     },
   },
